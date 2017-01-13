e79b0c594c766a306e0a0e8469a603fbb4c5c6ed
Chethan Pandarinath <chethan@crunch>
2017-01-12 21:39:52 -0800

    minor fix to missing util

--- src/+LFADS/+Utils/vectorMaskToIndices.m
+++ src/+LFADS/+Utils/vectorMaskToIndices.m
@@ -1,7 +1,7 @@
 function idx = vectorMaskToIndices(mask)
     if islogical(mask)
-        idx = makecol(find(mask));
+        idx = LFADS.Utils.makecol(find(mask));
     else
-        idx = makecol(mask);
+        idx = LFADS.Utils.makecol(mask);
     end
 end
