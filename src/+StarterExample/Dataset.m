84854cdeb97b003ff3338af4989c5ced764ce51f
Dan O'Shea <dan@djoshea.com>
2017-10-02 11:36:51 -0700

    Simplifying loadInfo in StarterExample.Dataset

--- src/+StarterExample/Dataset.m
+++ src/+StarterExample/Dataset.m
@@ -3,21 +3,28 @@ classdef Dataset < LFADS.Dataset
         function ds = Dataset(collection, relPath)
             ds = ds@LFADS.Dataset(collection, relPath);
         end
-        
-        function loadInfoFromData(ds, data)
-            % modify this to extract the metadata loaded from the data file
-            ds.subject = data.subject;
-            ds.saveTags = data.saveTags;
-            ds.datenum  = data.datenum;
-            ds.nChannels = data.nChannels;
-            ds.nTrials = data.nTrials;
-        end
-        
+
         function data = loadData(ds)
             % load this dataset's data file from .path
             in = load(ds.path);
             data = in.data;
         end
-        
+
+        function loadInfo(ds)
+            % Load this Dataset's metadata if not already loaded
+
+            if ds.infoLoaded, return; end
+
+            % modify this to extract the metadata loaded from the data file
+            data = ds.loadData();
+            ds.subject = data.subject;
+            ds.saveTags = data.saveTags;
+            ds.datenum  = data.datenum;
+            ds.nChannels = data.nChannels;
+            ds.nTrials = numel(data.trials);
+
+            ds.infoLoaded = true;
+        end
+
     end
 end
