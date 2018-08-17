093e353d5dd2147ca8e941cd2500e3ee3e2613ab
Dan O'Shea <dan@djoshea.com>
2018-08-16 17:16:38 -0700

    Better support for loading posterior means when some files are missing

--- src/+LFADS/+Utils/vectorMaskToIndices.m
+++ src/+LFADS/+Utils/vectorMaskToIndices.m
@@ -1,6 +1,6 @@
 function idx = vectorMaskToIndices(mask)
     if islogical(mask)
-        idx = LFADS.Utils.makecol(find(mask));
+        idx = LFADS.Utils.makecol(find(mask)); %#ok<FNDSB>
     else
         idx = LFADS.Utils.makecol(mask);
     end
