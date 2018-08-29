d6bd185e444b54738d7419b303e6876f51270f3f
Dan O'Shea <dan@djoshea.com>
2018-08-29 11:27:58 -0700

    Fixing issue with auto DISPLAY detection

--- src/+LFADS/+Utils/getDisplay.m
+++ src/+LFADS/+Utils/getDisplay.m
@@ -1,7 +1,15 @@
 function display = getDisplay()
     str = getenv('DISPLAY');
     if ~isempty(str)
-        display = str2double(str(2:end));
+        [~, rem] = strtok(str, ':');
+        if isempty(rem) || numel(rem) < 2
+            display = [];
+        else
+            display = str2double(rem(2:end));
+            if isnan(display)
+                display = [];
+            end
+        end
     else
         display = [];
     end
